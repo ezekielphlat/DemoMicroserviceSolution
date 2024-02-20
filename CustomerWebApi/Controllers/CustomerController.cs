8b8c833f0957bce6368c6798823630928b9e1677
Ezekiel Abodesegun <eabodesegun@appzonegroup.com>
2024-02-20 18:56:46 +0100

    add msql product service

---
+++ CustomerWebApi/Controllers/CustomerController.cs
@@ -0,0 +1,58 @@
+﻿using CustomerWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerDbContext _customerDbContext;
+
+        public CustomerController(CustomerDbContext customerDbContext)
+        {
+            this._customerDbContext = customerDbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetCustomers()
+        {
+            return _customerDbContext.Customers;
+
+        }
+
+        [HttpGet("{customerId:int}")]
+        public async Task<ActionResult<Customer>> GetCustomerByID(int customerID)
+        {
+            var customer = await _customerDbContext.Customers.FindAsync(customerID);
+            return customer;
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Customer customer)
+        {
+            await _customerDbContext.Customers.AddAsync(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok(customer);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(Customer customer)
+        {
+             _customerDbContext.Customers.Update(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok(customer);
+        }
+
+        [HttpDelete("{customerId:int}")]
+        public async Task<ActionResult> Delete(int customerId)
+        {
+            var customer = await _customerDbContext.Customers.FindAsync(customerId);
+            _customerDbContext.Customers.Remove(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
---
+++ ProductWebApi/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductWebApi.Models;
+
+namespace ProductWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext _productDbContext;
+
+        public ProductController(ProductDbContext productDbContext)
+        {
+            this._productDbContext = productDbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return _productDbContext.Products;
+
+        }
+
+        [HttpGet("{productId:int}")]
+        public async Task<ActionResult<Product>> GetProductByID(int ProductID)
+        {
+            var product = await _productDbContext.Products.FindAsync(ProductID);
+            return product;
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Product product)
+        {
+            await _productDbContext.Products.AddAsync(product);
+            await _productDbContext.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(Product product)
+        {
+            _productDbContext.Products.Update(product);
+            await _productDbContext.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpDelete("{productId:int}")]
+        public async Task<ActionResult> Delete(int productId)
+        {
+            var product = await _productDbContext.Products.FindAsync(productId);
+            _productDbContext.Products.Remove(product);
+            await _productDbContext.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
